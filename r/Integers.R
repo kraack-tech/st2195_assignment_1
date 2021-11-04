c032feb373162ccd7c2796dbc4e2800081e14576
kris-e2u <70644724+kris-e2u@users.noreply.github.com>
2021-11-04 21:53:58 +0100

    Add files via upload

---
+++ r/Integers.R
@@ -0,0 +1,20 @@
+---
+  title: "R-studio project"
+output: rmarkdown::github_document
+---
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+### Containing an R script that prints all integers between 1 and 10 (including 1 and 10) to screen
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricies nibh nec velit dictum luctus. Phasellus vulputate egestas dictum. Fusce euismod nibh et augue pulvinar.
+
+
+
+a<-seq(1,10)
+
+a
+
